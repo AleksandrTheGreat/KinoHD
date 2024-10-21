2a7b81a4df21c6724546c3b645e70933a5da9d9c
AleksandrTheGreat <267179@bk.ru>
2024-10-21 23:53:02 +0500

    Start project

--- KinoHD/apps.py
+++ KinoHD/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PiedpiperConfig(AppConfig):
+class KinohdConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PiedPiper'
+    name = 'KinoHD'
